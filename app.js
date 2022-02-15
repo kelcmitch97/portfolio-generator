dbeccf1665f599c76ae0faa6ca0dc87262c46817
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-02-15 15:46:09 -0330

    dynmically add portfolios and created page-template.js file

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err); //creates an exceptiona and stops the execution of the code
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
 
