305e04a39a827889e2cbbcc398863c1a381b8584
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-02-16 16:12:07 -0330

    prompt user response

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err; //creates an exceptiona and stops the execution of the code
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err); //creates an exceptiona and stops the execution of the code
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
