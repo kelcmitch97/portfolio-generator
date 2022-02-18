7181cc6eb467a2f6f434b35f95be7356b0df8a45
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-02-17 21:03:25 -0330

    Finished site

---
+++ utils/generate-site.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return; //need to return out of function if rejected
+            }
+
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+const copyFile = () => {
+    return new Promise ((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+              reject(err);
+              return;
+            }
+      
+            resolve({
+              ok: true,
+              message: 'Stylesheet created!'
+            });
+          });
+        });
+};
+
+// export object with 2 functions
+module.exports= {
+    writeFile,
+    copyFile
+};
